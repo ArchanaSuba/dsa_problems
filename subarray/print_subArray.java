c1a626e50ffb1290818ff278d5c242c53fc037da
ArchanaSuba <archanasuba.94@gmail.com>
2025-05-23 23:28:24 +0530

    Printsubarray

--- subarray/print_subArray.java
+++ subarray/print_subArray.java
@@ -1,7 +1,34 @@
 package subarray;
 
-public class print_subArray {
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class print_subArray {
+
+public static void printsubarray(int arr[]){
+    int n = arr.length;
+    System.out.println();
+    for(int s =0;s<n;s++){
+        for(int e = s;e<n;e++){
+            for(int k = s;k<=e;k++){
+                System.out.print(arr[k]);
+            }
+            System.out.println();
+        }
+    }
+}
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array size");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for(int i =0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Entered array is ....");
+        for(int j=0;j<size;j++){
+            System.out.print(arr[j]);
+        }
+        printsubarray(arr);
     }
 }
